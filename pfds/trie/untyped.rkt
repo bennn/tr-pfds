e1efd11bfb296fcc52f7099343631528ac5c470a
ben <types@ccs.neu.edu>
2016-01-09 19:12:12 -0500

    private/trie exports safe & unsafe identifiers

--- pfds/trie/untyped.rkt
+++ pfds/trie/untyped.rkt
@@ -1,17 +1,27 @@
 #lang racket/base
-(require (prefix-in : "../private/unsafe-trie.rkt")
-         (only-in "../private/unsafe-trie.rkt" Mt?)
-         racket/contract/base)
+
+(require
+  racket/contract/base)
+
+(require
+  (only-in "../private/unsafe-trie.rkt"
+    Mt?)
+  (only-in "../private/unsafe-trie.rkt"
+    unsafe-lookup
+    unsafe-bind
+    unsafe-trie
+    unsafe-insert
+    unsafe-tries))
 
 (define-values (mk-Trie Trie-real Trie?)
   (let ()
     (struct Trie (real)) 
     (values Trie Trie-real Trie?)))
 
-(define (lookup keys map) (:lookup keys (Trie-real map)))
-(define (bind lok v map) (mk-Trie (:bind lok v (Trie-real map))))
-(define (trie lst) (mk-Trie (:trie lst)))
-(define (insert lstv lstk tri) (mk-Trie (:insert lstv lstk (Trie-real tri))))
+(define (lookup keys map) (unsafe-lookup keys (Trie-real map)))
+(define (bind lok v map) (mk-Trie (unsafe-bind lok v (Trie-real map))))
+(define (trie lst) (mk-Trie (unsafe-trie lst)))
+(define (insert lstv lstk tri) (mk-Trie (unsafe-insert lstv lstk (Trie-real tri))))
 (define (tries lstv lstk) (mk-Trie (tries lstv lstk)))
 (provide
  (contract-out
